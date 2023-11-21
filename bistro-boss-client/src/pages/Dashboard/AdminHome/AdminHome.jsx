f41ffab679bb3f0cefdcdde41293e6847d74ada7
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-11-21 17:01:53 +0600

    feat(ui): create a component for admis stats

--- bistro-boss-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ bistro-boss-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,17 +1,169 @@
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query"
+import { FaAddressBook, FaDollarSign, FaListAlt, FaUsers } from 'react-icons/fa'
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid,
+    PieChart, Pie, Sector, ResponsiveContainer, Legend,
+} from 'recharts';
 
+
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 
 const AdminHome = () => {
     const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/admin-stats");
+            return res.data;
+        }
+    })
+
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['order-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/order-stats");
+            return res.data;
+        }
+    })
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+                          ${x + width / 2}, ${y}
+                          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+                          Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
+    // custom pie chart
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const pieCharData = chartData.map(data => {
+        return {name: data.category, value: data.revenue}
+    })
+
     return (
         <div>
             <h2 className='text-2xl text-center'>
                 <span>Hi, Welcome</span>
                 {
-                    user?.displayName ? user?.displayName : "back!"
+                    user?.displayName ? " " + user?.displayName : "back!"
                 }
             </h2>
+
+            {/* stats */}
+            <div>
+                <div className="stats shadow">
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaDollarSign className="text-4xl"></FaDollarSign>
+                        </div>
+                        <div className="stat-title">Revenue</div>
+                        <div className="stat-value">${stats.revenue}</div>
+                        <div className="stat-desc">Jan 1st - Feb 1st</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaUsers className="text-4xl"></FaUsers>
+                        </div>
+                        <div className="stat-title">Users</div>
+                        <div className="stat-value">{stats.users}</div>
+                        <div className="stat-desc">↗︎ 400 (22%)</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaListAlt className="text-4xl"></FaListAlt>
+                        </div>
+                        <div className="stat-title">Menu Items</div>
+                        <div className="stat-value">{stats.menuItems}</div>
+                        <div className="stat-desc">↗︎ 400 (22%)</div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaAddressBook className="text-4xl"></FaAddressBook>
+                        </div>
+                        <div className="stat-title">Orders</div>
+                        <div className="stat-value">{stats.orders}</div>
+                        <div className="stat-desc">↗︎ 400 (22%)</div>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+            <div className="flex">
+                <div className="1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+
+                    {/* App.demoUrl = 'https://codesandbox.io/s/bar-chart-with-customized-shape-dusth'; */}
+
+                </div>
+                <div className="1/2">                    
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                data={pieCharData}
+                                cx="50%"
+                                cy="50%"
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {pieCharData.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                            <Legend></Legend>
+                        </PieChart>                    
+                </div>
+            </div>
         </div>
     );
 };
